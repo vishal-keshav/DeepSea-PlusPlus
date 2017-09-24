afa5a757d31d9b7dfab0c401c493d9f585c165d0
bulletcross <bulletcross@gmail.com>
2017-09-24 13:14:35 +0530

    broadcasting fixed

--- DeepSea/main.cpp
+++ DeepSea/main.cpp
@@ -2,13 +2,23 @@
 #include "math_util.h"
 #include <blaze/Math.h>
 
+#define MATRIX blaze::DynamicMatrix<double>
+#define print std::cout<<
 int main()
 {
-    blaze::DynamicMatrix<double> mat1(3,3,1);
-    blaze::DynamicMatrix<double> mat2(3,3,2);
-    blaze::DynamicMatrix<double> mat3 = add(mat1, mat2);
+    MATRIX mat1(3,3,1);
+    MATRIX mat2(3,3,2);
+    MATRIX mat3 = add(mat1, mat2);
+    print mat3 ;
 
-    std::cout << mat3 << std::endl;
+    MATRIX vec1(3,1,5);
+    MATRIX mat5 = add(mat1, vec1);
+    print mat5 ;
+
+    MATRIX vec2(4,1,9);
+    MATRIX scalar(1,1,5);
+    MATRIX vec3 = subs(vec2,scalar);
+    print vec3 ;
 
     return 0;
 }
--- DeepSea/math_util.h
+++ DeepSea/math_util.h
@@ -10,11 +10,16 @@ DynamicMatrix<double> add(DynamicMatrix<double> A, DynamicMatrix<double> B){
 		return A+B;
 	}
 	else if(A.rows() > B.rows() && B.rows()==1 && A.columns()==1 && B.columns()==1){
-		DynamicMatrix<double> C(A.rows(),1, 0);
+		DynamicMatrix<double> C(A.rows(),1, B(0,0));
 		return A+C;
 	}
 	else if(A.rows() == B.rows() && A.columns() > B.columns() && B.columns()==1){
-		DynamicMatrix<double> C(A.rows(), A.columns(), 0);
+		DynamicMatrix<double> C(A.rows(), A.columns());
+		for(int i=0;i<C.rows(); i++){
+            for(int j=0;j<C.columns();j++){
+                C(i,j) = B(i,0);
+            }
+		}
 		return A+C;
 	}
 	else{
@@ -33,11 +38,16 @@ DynamicMatrix<double> subs(DynamicMatrix<double> A, DynamicMatrix<double> B){
 		return A-B;
 	}
 	else if(A.rows() > B.rows() && B.rows()==1 && A.columns()==1 && B.columns()==1){
-		DynamicMatrix<double> C(A.rows(),1, 0);
+		DynamicMatrix<double> C(A.rows(),1, B(0,0));
 		return A-C;
 	}
 	else if(A.rows() == B.rows() && A.columns() > B.columns() && B.columns()==1){
-		DynamicMatrix<double> C(A.rows(), A.columns(),0);
+		DynamicMatrix<double> C(A.rows(), A.columns());
+		for(int i=0;i<C.rows(); i++){
+            for(int j=0;j<C.columns();j++){
+                C(i,j) = B(i,0);
+            }
+		}
 		return A-C;
 	}
 	else{
