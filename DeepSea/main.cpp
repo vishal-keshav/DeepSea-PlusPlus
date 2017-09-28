b6dfe65b5f2330ce01f06a490dfd3e0b25d1cc9a
bulletcross <bulletcross@gmail.com>
2017-09-28 23:57:23 +0530

    data and train template added

--- DeepSea/main.cpp
+++ DeepSea/main.cpp
@@ -5,71 +5,42 @@
 #include "prediction.h"
 #include <blaze/Math.h>
 #include <vector>
-//#define MATRIX blaze::DynamicMatrix<double>
-//#define print std::cout<<
 
 using namespace std;
 int main()
 {
-    /*MATRIX mat1(3,3,1);
-    MATRIX mat2(3,3,2);
-    MATRIX mat3 = add(mat1, mat2);
-    print mat3 ;
-
-    MATRIX vec1(3,1,5);
-    MATRIX mat5 = add(mat1, vec1);
-    print mat5 ;
-
-    MATRIX vec2(4,1,9);
-    MATRIX scalar(1,1,5);
-    MATRIX vec3 = subs(vec2,scalar);
-    print vec3 ;*/
-    static const int arr[] = {3,5,4};
-    vector<int> layer (arr, arr + sizeof(arr) / sizeof(arr[0]) );
-    model_param test(layer);
-    //test.print_weight();
-    //test.print_bias();
-    initialize_param(&test);
-    //test.print_weight();
-    //test.print_bias();
-    //cout << apply_relu(test.W[0]) << endl;
-    //cout << apply_sigmoid(test.W[1]) << endl;
-
-    blaze::DynamicMatrix<double> X = read_csv("sample_data1.csv", 3, 3);
-    blaze::DynamicMatrix<double> Y = read_csv("sample_data2.csv", 4, 3);
-    //cout << apply_log(D) << endl;
-    //cout << apply_softmax(D) << endl;
-    //cout << mean_cross_entropy_loss(D, apply_softmax(D)) << endl;
-    forward_param f_test(layer,3);
-    //f_test.print_linear();
-    cout << endl;
-    //f_test.print_activated();
-    cout << endl;
-    feed_forward(&test, &f_test, X);
-    //f_test.print_linear();
-    cout << endl;
-    //f_test.print_activated();
-    backward_param b_test(layer,3);
-
-    back_prop(&test,&f_test,&b_test,Y);
-    gradient_descent(&test, &b_test, 0.2);
-    write_model(&test, "model_1.txt");
-    model_param m_p = read_model("model_1.txt");
-
-    //Initialize variables, decide on batch size[DONE]
-    //For each epoch[DONE]
-    //Retrieve batch[TODO]
-    //forward pass[DONE]
-    //Backward pass[DONE]
-    //Write model[DONE]
-
-    //Read model[DONE]
-    //output = predict(input,m_p)[DONE]
-
-    DynamicMatrix<double> pred = predict(&m_p, &f_test, X);
-    cout << pred << endl;
-    cout << accuracy(Y, pred) << endl;
-    cout << mean_cross_entropy_loss(Y, pred) << endl;
+	//Define layer structure
+	static const int arr[] = {16, 20, 35, 10, 26};
+	vector<int> layer (arr, arr + sizeof(arr)/sizeof(arr[0]));
+	
+	//Declare training params
+	int batch_size = 100;
+	int nr_epoch = 10;
+	double learning_rate = 0.2;
+	
+	//Intialize model and train graph parameters
+	model_param m_p(layer);
+	initialize_param(&m_p);
+	
+	forward_param f_p(layer, batch_size);
+	backward_param b_p(layer, batch_size);
+	
+	//read datafile, seperate test data X_test, Y_test
+	
+	//Start training
+	for(int i=0;i<nr_epoch;i++){
+		nr_batch = ; //Based on read datafile
+		for(int j=0;j<nr_batch;j++){
+			//Preapare input batch X and output label Y
+			feed_forward(&m_p, &f_p, X);
+			back_prop(&m_p, &f_p, &b_p, Y);
+			gradient_descent(&m_p, &b_p, learning_rate);
+		}
+		//Print accuracy and cost on test data set
+		O = predict(&m_p, &f_p, X_test);
+		cout << "Cost for epoch " << i << " is " << mean_cross_entropy_loss(Y_test, O) << endl;
+		cout << "Accuracy for epoch " << i << " is " << accuracy(Y_test, O) << endl;
+	}
 
     return 0;
 }
